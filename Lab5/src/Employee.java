ebe5b92f8368262c32fe3b10ad6bd1037afe0e51
Naim-Ul Hossain Anik <125190323+nnaimanik221-15-5988@users.noreply.github.com>
2023-02-18 22:27:29 +0600

    Add files via upload

---
+++ Lab5/src/Employee.java
@@ -0,0 +1,18 @@
+
+public class Employee
+{
+    int id;
+    String name;
+
+    public Employee(int a, String nam)
+    {
+        this.id = a;
+        this.name = nam;
+    }
+
+    public void info()
+    {
+        System.out.println("\nName: " + name + "\nId: " + id);
+    }
+}
+
---
+++ Lab5/src/Main.java
@@ -0,0 +1,11 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Employee obj1 = new Employee(1, "Anik");
+        Employee obj2 = new Employee(2, "Naim");
+
+        obj1.info();
+        obj2.info();
+    }
+}
